5fbad7e609a47f8673a2632c5ec7665823a3661d
Vishal Tyagi <visualtaggy@gmail.com>
2022-03-28 01:18:55 +0530

    'points' to remember

--- Theory/Pointers/References_in_cpp.cpp
+++ Theory/Pointers/References_in_cpp.cpp
@@ -24,4 +24,9 @@ int main()
     magic_increment(b);
     cout << b << endl;
     return 0;
+
+    // Rules to Remember
+    //  1. Decleration and Initialization should be done at the same time.
+    //  2. Can't  re-initialize to something else, if it's a ref to a then can't be changed to b (this will end up storing value of B)
+    //  3. Must have a valid memory address i.e can't store NULL memory address to it
 }
