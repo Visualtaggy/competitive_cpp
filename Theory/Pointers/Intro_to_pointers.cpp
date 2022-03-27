83f796d786814dfc61f6652498037b058f0f8e0f
Vishal Tyagi <visualtaggy@gmail.com>
2022-03-28 00:23:42 +0530

    basics on pointers

---
+++ Theory/Pointers/Intro_to_pointers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int a = 10;
+
+    int *b = &a; // storing address of a inside pointer b
+
+    cout << a << endl;  // 10
+    cout << &a << endl; // address  of a
+    cout << b << endl;  // address of a
+    cout << *b << endl; // dereference operator, hence value of a
+    cout << &b << endl; // base address of a
+    return 0;
+}
