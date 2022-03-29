92c50f1ec7ac0131e61f4b089abd517134e09948
Vishal Tyagi <visualtaggy@gmail.com>
2022-03-29 10:42:23 +0530

    Reference type though variables

--- Theory/Pointers/PassingByValue.cpp
+++ Theory/Pointers/PassingByValue.cpp
@@ -17,6 +17,14 @@ void swapByRef(int *a, int *b)
     cout << "values inside function: " << *a << " " << *b << endl;
 }
 
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+    cout << "values inside function: " << *a << " " << *b << endl;
+}
+
 int main()
 {
     int a = 10;
@@ -30,6 +38,13 @@ int main()
     cout << "Swap by Referance" << endl;
     swapByRef(&a, &b);
     cout << "values: " << a << " " << b << endl;
+
+    cout << "=================================================================" << endl;
+    cout << "Swap by Referance though variables" << endl;
+    int *r1 = &a;
+    int *r2 = &b;
+    swap(r1, r2);
+    cout << "values:" << a << " " << b << endl;
 
     return 0;
 }
