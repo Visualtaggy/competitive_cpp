de1e6d667acd13848234701a6f9660a0a6f19f01
Vishal Tyagi <visualtaggy@gmail.com>
2022-03-29 19:55:53 +0530

    [Soln]: Print Z

---
+++ Basics Of Programming/Getting Started/Print Z.cpp
@@ -0,0 +1,15 @@
+// 1. You are required to print a 'z' of size 5 using '*'.
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "*****" << endl;
+    cout << "   *" << endl;
+    cout << "  *" << endl;
+    cout << " *" << endl;
+    cout << "*****" << endl;
+    return 0;
+}
