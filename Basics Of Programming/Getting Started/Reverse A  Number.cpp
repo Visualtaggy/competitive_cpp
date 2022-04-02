d2f0edb024e2091063ac1504b8247cd208bded47
Vishal Tyagi <visualtaggy@gmail.com>
2022-04-02 15:27:24 +0530

    [Soln] Reverse of a number

---
+++ Basics Of Programming/Getting Started/Reverse A  Number.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number;
+    cin >> number;
+
+    while (number != 0)
+    {
+        cout << number % 10 << endl;
+        number = number / 10;
+    }
+    return 0;
+}
