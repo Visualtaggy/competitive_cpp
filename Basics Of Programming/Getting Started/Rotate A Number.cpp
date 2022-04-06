d5f790ce87ecd83b13721c8393f7cee625793606
Vishal Tyagi <visualtaggy@gmail.com>
2022-04-06 14:30:04 +0530

    [Soln] Rotate numbers by K

---
+++ Basics Of Programming/Getting Started/Rotate A Number.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+
+    int nod = 0;
+    int temp = n;
+    while (temp != 0)
+    {
+        temp = temp / 10;
+        ++nod;
+    }
+
+    if (k < 0)
+    {
+        k = k + nod;
+    }
+
+    k = k % nod;
+    int div = 1;
+    int mult = 1;
+    int total = 0;
+    for (int i = 1; i <= nod; i++)
+    {
+        if (i <= k)
+        {
+            div = div * 10;
+        }
+        else
+        {
+            mult = mult * 10;
+        }
+    }
+    int r = n % div;
+    int q = n / div;
+
+    total = (r * mult) + q;
+    cout << total << endl;
+    return 0;
+}
