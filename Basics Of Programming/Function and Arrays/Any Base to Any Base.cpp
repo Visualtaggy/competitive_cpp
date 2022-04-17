35687934c5a4e7298ebc6a1171c7694354639f54
Vishal Tyagi <visualtaggy@gmail.com>
2022-04-17 13:17:26 +0530

    [quest] any base to any base

---
+++ Basics Of Programming/Function and Arrays/Any Base to Any Base.cpp
@@ -0,0 +1,23 @@
+// 1. You are given a number n.
+// 2. You are given a base b1. n is a number on base b.
+// 3. You are given another base b2.
+// 4. You are required to convert the number n of base b1 to a number in base b2.
+
+#include <iostream>
+using namespace std;
+public
+static int AnyToAny(int n, int b1, int b2)
+{
+    // write your code here
+}
+int main()
+{
+    int n;
+    int b1;
+    int b2;
+    cin >> n;
+    cin >> b1;
+    cin >> b2;
+    int res = AnyToAny(n, b1, b2);
+    cout << res << endl;
+}
