b1017b37b31858fc344ae7c4a175b34b81bf0e3d
Vishal Tyagi <visualtaggy@gmail.com>
2022-04-17 12:02:58 +0530

    [Quest]Any base to decimal

---
+++ Basics Of Programming/Function and Arrays/Any Base to Decimal.cpp
@@ -0,0 +1,20 @@
+// 1. You are given a decimal number n.
+// 2. You are given a base b.
+// 3. You are required to convert the number n into its corresponding value in base b.
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int DecToAny(int n, int b)
+{
+    // write your code here
+}
+int main()
+{
+    int n;
+    int b;
+    cin >> n;
+    cin >> b;
+    int res = DecToAny(n, b);
+    cout << res << endl;
+}
