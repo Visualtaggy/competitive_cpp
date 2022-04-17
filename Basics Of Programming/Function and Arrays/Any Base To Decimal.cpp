158ee032c564d5b269e6303453ae28639cadf6c4
Vishal Tyagi <visualtaggy@gmail.com>
2022-04-17 13:08:03 +0530

    [Soln] Any base to decimal

--- Basics Of Programming/Function and Arrays/Any Base To Decimal.cpp
+++ Basics Of Programming/Function and Arrays/Any Base To Decimal.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 int AnyToDec(int n, int b)
 {
   // write your code here.
+
+  int power = 0;
+  int total = 0;
+
+  while (n != 0)
+  {
+    int digit = n % 10;
+    n = n / 10;
+    total += pow(b, power) * digit;
+    ++power;
+  }
+
+  return total;
 }
 int main()
 {
