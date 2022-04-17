2ade474b93fc75f06d27f9758209b1a7ac483556
Vishal Tyagi <visualtaggy@gmail.com>
2022-04-17 12:46:40 +0530

    [Quest] Any base to decimal

---
+++ Basics Of Programming/Function and Arrays/Any Base To Decimal.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int AnyToDec(int n, int b)
+{
+  // write your code here.
+}
+int main()
+{
+  int n;
+  int b;
+  cin >> n;
+  cin >> b;
+  int res = AnyToDec(n, b);
+  cout << res << endl;
+}
