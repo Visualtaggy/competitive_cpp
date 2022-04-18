d8a4e4351592ca882441571c7ae65136dcabe2ca
Vishal Tyagi <visualtaggy@gmail.com>
2022-04-18 14:08:19 +0530

    [Soln] Any base to any base conversion

--- Basics Of Programming/Function and Arrays/Any Base to Any Base.cpp
+++ Basics Of Programming/Function and Arrays/Any Base to Any Base.cpp
@@ -4,11 +4,17 @@
 // 4. You are required to convert the number n of base b1 to a number in base b2.
 
 #include <iostream>
+#include <cmath>
 using namespace std;
-public
-static int AnyToAny(int n, int b1, int b2)
+
+int AnyToDecimal(int n, int b);
+int DecimalToAnyBase(int n, int b);
+
+int AnyToAny(int n, int b1, int b2)
 {
     // write your code here
+    int decimalValue = AnyToDecimal(n, b1);
+    return DecimalToAnyBase(decimalValue, b2);
 }
 int main()
 {
@@ -21,3 +27,31 @@ int main()
     int res = AnyToAny(n, b1, b2);
     cout << res << endl;
 }
+int AnyToDecimal(int n, int b)
+{
+    int power = 0;
+    int total = 0;
+
+    while (n != 0)
+    {
+        int digit = n % 10;
+        n = n / 10;
+        total += pow(b, power) * digit;
+        ++power;
+    }
+    return total;
+};
+
+int DecimalToAnyBase(int n, int b)
+{
+    int total = 0;
+    int power = 0;
+    while (n != 0)
+    {
+        int digit = n % b;
+        n = n / b;
+        total += pow(10, power) * digit;
+        ++power;
+    }
+    return total;
+};
