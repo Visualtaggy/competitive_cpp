d13ba01ade3e25c6fa42c20722683ce6cbc6f3aa
Vishal Tyagi <visualtaggy@gmail.com>
2022-04-21 21:57:30 +0530

    [Quest] Any Base subtration

---
+++ Basics Of Programming/Function and Arrays/Any Base Subtraction.cpp
@@ -0,0 +1,17 @@
+// 1. You are given a base b.
+// 2. You are given two numbers n1 and n2 of base b.
+// 3. You are required to subtract n1 from n2 and print the value.
+#include <iostream>
+using namespace std;
+
+int getDifference(int b, int n1, int n2)
+{
+    // write your code here
+}
+
+int main()
+{
+    int b, n1, n2;
+    cin >> b >> n1 >> n2;
+    getDifference(b, n1, n2);
+}
