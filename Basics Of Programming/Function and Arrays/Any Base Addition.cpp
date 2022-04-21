630df2a035a5128e1b3e970d71c9cc1b8ceaa00e
Vishal Tyagi <visualtaggy@gmail.com>
2022-04-21 21:55:22 +0530

    [Soln] Any base addition

--- Basics Of Programming/Function and Arrays/Any Base Addition.cpp
+++ Basics Of Programming/Function and Arrays/Any Base Addition.cpp
@@ -3,12 +3,29 @@
 // 3. You are required to add the two numbes and print their value in base b.
 
 #include <iostream>
-
+#include <cmath>
 using namespace std;
 
 int getSum(int b, int n1, int n2)
 {
-    // write your code here
+    int answer = 0;
+    int carry = 0;
+    int power = 0;
+
+    while (n1 != 0 || n2 != 0 || carry != 0)
+    {
+        int d1 = n1 % 10;
+        int d2 = n2 % 10;
+
+        int sum = d1 + d2 + carry;
+        carry = sum / b;
+        sum = sum % b;
+        answer += pow(10, power) * sum;
+        ++power;
+        n1 = n1 / 10;
+        n2 = n2 / 10;
+    }
+    return answer;
 }
 
 int main()
