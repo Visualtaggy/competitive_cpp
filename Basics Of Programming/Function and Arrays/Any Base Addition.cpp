cd144eebe3d14e329a0d0895f18430cb1030dd37
Vishal Tyagi <visualtaggy@gmail.com>
2022-04-21 18:35:27 +0530

    [Quest] Any base addition

---
+++ Basics Of Programming/Function and Arrays/Any Base Addition.cpp
@@ -0,0 +1,19 @@
+// 1. You are given a base b.
+// 2. You are given two numbers n1 and n2 of base b.
+// 3. You are required to add the two numbes and print their value in base b.
+
+#include <iostream>
+
+using namespace std;
+
+int getSum(int b, int n1, int n2)
+{
+    // write your code here
+}
+
+int main()
+{
+    int b, n1, n2;
+    cin >> b >> n1 >> n2;
+    cout << getSum(b, n1, n2) << endl;
+}
