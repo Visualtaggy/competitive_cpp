ce4de64b82f1a9aec74ce00baa20e55adc5f70f6
Vishal Tyagi <visualtaggy@gmail.com>
2022-04-22 10:05:20 +0530

    [Quest] Any base multiplication

---
+++ Basics Of Programming/Function and Arrays/Any Base Multiplication.cpp
@@ -0,0 +1,18 @@
+// 1. You are given a base b.
+// 2. You are given two numbers n1 and n2 of base b.
+// 3. You are required to multiply n1 and n2 and print the value.
+
+#include <iostream>
+using namespace std;
+
+int getProduct(int b, int n1, int n2)
+{
+    // write your code here
+}
+
+int main()
+{
+    int b, n1, n2;
+    cin >> b >> n1 >> n2;
+    cout << getProduct(b, n1, n2) << endl;
+}
