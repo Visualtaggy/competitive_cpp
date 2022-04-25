b4d2962567789932517faeee64a21ac69ccb1ef6
Vishal Tyagi <visualtaggy@gmail.com>
2022-04-25 11:01:30 +0530

    [FIX] Fixing bug

--- Basics Of Programming/Function and Arrays/Any Base Multiplication.cpp
+++ Basics Of Programming/Function and Arrays/Any Base Multiplication.cpp
@@ -5,7 +5,9 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
+int getSum(int b, int n1, int n2);
 int getProductSingle(int b, int n1, int d2);
+
 int getProduct(int b, int n1, int n2)
 {
     // write your code here
@@ -15,9 +17,31 @@ int getProduct(int b, int n1, int n2)
     {
         int d2 = n2 % 10;
         int digit = getProductSingle(b, n1, d2);
-        answer += pow(10, power) * digit;
+        answer = getSum(b, answer, digit * pow(10, power));
         ++power;
+        n2 = n2 / 10;
+    }
 
+    return answer;
+}
+
+int getSum(int b, int n1, int n2)
+{
+    int answer = 0;
+    int carry = 0;
+    int power = 0;
+
+    while (n1 != 0 || n2 != 0 || carry != 0)
+    {
+        int d1 = n1 % 10;
+        int d2 = n2 % 10;
+
+        int sum = d1 + d2 + carry;
+        carry = sum / b;
+        sum = sum % b;
+        answer += pow(10, power) * sum;
+        ++power;
+        n1 = n1 / 10;
         n2 = n2 / 10;
     }
     return answer;
@@ -32,7 +56,7 @@ int getProductSingle(int b, int n1, int d2)
     {
         int d1 = n1 % 10;
 
-        int digit = d1 * d2 + carry;
+        int digit = (d1 * d2) + carry;
 
         carry = digit / b;
         digit = digit % b;
