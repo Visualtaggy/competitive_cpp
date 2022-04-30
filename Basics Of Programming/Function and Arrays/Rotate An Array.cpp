cd49124e47d92020575d78dc0ec731359dd41c43
Vishal Tyagi <visualtaggy@gmail.com>
2022-05-01 01:16:37 +0530

    [Quest] Rotate an array

---
+++ Basics Of Programming/Function and Arrays/Rotate An Array.cpp
@@ -0,0 +1,37 @@
+// 1. You are given a number n, representing the size of array a.
+// 2. You are given n numbers, representing elements of array a.
+// 3. You are given a number k.
+// 4. Rotate the array a, k times to the right (for positive values of k), and to
+// the left for negative values of k.
+
+#include <iostream>
+using namespace std;
+
+void rotate(int *arr, int n, int k)
+{
+    // write your code here
+}
+
+void display(int *arr, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int n, r;
+    cin >> n;
+    int *arr = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    cin >> r;
+
+    rotate(arr, n, r);
+    display(arr, n);
+}
