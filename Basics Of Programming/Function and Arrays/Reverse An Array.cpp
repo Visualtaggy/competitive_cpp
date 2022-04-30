a397649e70d3a1c7ff654217bdfb42ab925e8afc
Vishal Tyagi <visualtaggy@gmail.com>
2022-05-01 00:26:30 +0530

    [Quest] Reverse an array

---
+++ Basics Of Programming/Function and Arrays/Reverse An Array.cpp
@@ -0,0 +1,34 @@
+// 1. You are given a number n, representing the size of array a.
+// 2. You are given n numbers, representing elements of array a.
+// 3. You are required to reverse the contents of array a.
+
+#include <iostream>
+using namespace std;
+
+void reverse(int *arr, int n)
+{
+    // write your code here
+}
+
+void display(int *arr, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int *arr = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    reverse(arr, n);
+    display(arr, n);
+}
