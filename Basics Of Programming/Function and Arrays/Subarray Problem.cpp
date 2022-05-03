2bbdbb817b0d1906b86874d3e7ca7c6bdf44130c
Vishal Tyagi <visualtaggy@gmail.com>
2022-05-03 20:59:49 +0530

    [Quest] Added code

--- Basics Of Programming/Function and Arrays/Subarray Problem.cpp
+++ Basics Of Programming/Function and Arrays/Subarray Problem.cpp
@@ -1,3 +1,19 @@
 // 1. You are given an array of size 'n' and n elements of the same array.
 // 2. You are required to find and print all the subarrays of the given array.
 // 3. Each subarray should be space seperated and on a seperate lines. Refer to sample input and output.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int *arr = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    // write your code here
+}
