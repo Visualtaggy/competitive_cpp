fd8e55e7b39b40c2a3fa2341afbc856c733cf7e0
Vishal Tyagi <visualtaggy@gmail.com>
2022-05-05 12:05:20 +0530

    [Soln]: Subarray problem

--- Basics Of Programming/Function and Arrays/Subarray Problem.cpp
+++ Basics Of Programming/Function and Arrays/Subarray Problem.cpp
@@ -16,4 +16,17 @@ int main()
     }
 
     // write your code here
+
+    for (int start = 0; start < n; start++)
+    {
+        for (int end = start; end < n; end++)
+        {
+            for (int printVal = start; printVal <= end; printVal++)
+            {
+                cout << arr[printVal] << "\t";
+            }
+             cout << endl;
+        }
+       
+    }
 }
