cfbff9f784d78ad818157a367b8f0b41e70c2889
Vishal Tyagi <visualtaggy@gmail.com>
2022-05-13 18:33:59 +0530

    [Soln] Subsets of an array

--- Basics Of Programming/Function and Arrays/Subsets Of Array.cpp
+++ Basics Of Programming/Function and Arrays/Subsets Of Array.cpp
@@ -4,6 +4,8 @@
 // on separate line. For more clarity check out sample input and output.
 
 #include <iostream>
+#include <cmath>
+#include <string>
 using namespace std;
 int main()
 {
@@ -15,6 +17,30 @@ int main()
     for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
+    }
+
+    int superSets = pow(2, n);
+
+    for (int i = 0; i < superSets; i++)
+    {
+        int temp = i;
+        string answer = "";
+
+        for (int j = n - 1; j >= 0; j--)
+        {
+            int digit = temp % 2;
+            temp = temp / 2;
+
+            if (digit == 0)
+            {
+                answer = "-\t" + answer;
+            }
+            else
+            {
+                answer = to_string(arr[j]) + "\t" + answer;
+            }
+        }
+        cout << answer << endl;
     }
 
     return 0;
