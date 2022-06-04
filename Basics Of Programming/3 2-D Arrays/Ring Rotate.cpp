44269df4b04a04e7a489580002366b31fb0334f5
Vishal Tyagi <visualtaggy@gmail.com>
2022-06-04 17:53:23 +0530

    logic to extract shell from 2d array

--- Basics Of Programming/3 2-D Arrays/Ring Rotate.cpp
+++ Basics Of Programming/3 2-D Arrays/Ring Rotate.cpp
@@ -10,10 +10,11 @@ using namespace std;
 
 // functions
 void display(vector<vector<int>> arr, int r, int c);
+vector<int> extractShell(vector<vector<int>> arr, int shell);
 
 int main()
 {
-    int r, c;
+    int r, c, shell, rotate;
     cin >> r >> c;
 
     vector<vector<int>> arr;
@@ -33,7 +34,55 @@ int main()
 
     // Code here
     display(arr, r, c);
+    cin >> shell;
+    extractShell(arr, shell);
+
     return 0;
+}
+
+vector<int> extractShell(vector<vector<int>> arr, int shell)
+{
+    vector<int> extractedShell;
+    int minR = shell - 1;
+    int minC = shell - 1;
+    int maxR = arr.size() - shell;
+    int maxC = arr[0].size() - shell;
+
+    // THIS IS NOT NEEDED FOR VECTORS BUT will be needed if you use arrays and need to find size on 1D array
+    //   int totalElements = 2 * (maxR - minR + maxC - minC); // high  - low + 1 to get total number of elements
+
+    // left wall
+    for (int i = minR, j = minC; i <= maxR; i++)
+    {
+        extractedShell.push_back(arr[i][j]);
+    }
+    ++minC; // to avoid repeat
+
+    // botton wall
+    for (int i = maxR, j = minC; j <= maxC; j++)
+    {
+        extractedShell.push_back(arr[i][j]);
+    }
+    --maxR;
+    // right wall
+    for (int i = maxR, j = maxC; i >= minR; i--)
+    {
+        extractedShell.push_back(arr[i][j]);
+    }
+    --maxC;
+    // top wall
+    for (int i = minR, j = maxC; j >= minC; j--)
+    {
+        extractedShell.push_back(arr[i][j]);
+    }
+
+    // DEBUG PRINT
+    // for (int i = 0; i < extractedShell.size(); i++)
+    // {
+    //     cout << extractedShell[i] << " ";
+    // }
+
+    return extractedShell;
 }
 
 void display(vector<vector<int>> arr, int r, int c)
