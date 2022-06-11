e6407f924f5e8b86c0614d6b6c88293de68d77f2
Vishal Tyagi <visualtaggy@gmail.com>
2022-06-12 01:06:14 +0530

    [Soln] Logic to print all substrings

--- Basics Of Programming/4 String StringBuilder ArrayList/Print All Palindromic Substrings.cpp
+++ Basics Of Programming/4 String StringBuilder ArrayList/Print All Palindromic Substrings.cpp
@@ -1,2 +1,25 @@
 // 1. You are given a string.
 // 2. You have to print all palindromic substrings of the given string.
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    // Taking input
+    string str;
+    getline(cin, str);
+
+    for (int i = 0; i < str.size(); i++)
+    {
+        // j + i since the total of final string of the loop always comes to 4 for abcc from i -> j
+        for (int j = 1; j + i <= str.size(); j++)
+        {
+            cout << str.substr(i, j) << endl;
+        }
+    }
+
+    return 0;
+}
