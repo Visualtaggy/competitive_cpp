c8c87374a53dfb00eaadce425a070ad0ac81292e
Vishal Tyagi <visualtaggy@gmail.com>
2022-06-12 10:22:34 +0530

    [Soln] Added check palindrome function

--- Basics Of Programming/4 String StringBuilder ArrayList/Print All Palindromic Substrings.cpp
+++ Basics Of Programming/4 String StringBuilder ArrayList/Print All Palindromic Substrings.cpp
@@ -6,6 +6,8 @@
 
 using namespace std;
 
+bool isPalindrome(string str);
+
 int main()
 {
     // Taking input
@@ -17,9 +19,28 @@ int main()
         // j + i since the total of final string of the loop always comes to 4 for abcc from i -> j
         for (int j = 1; j + i <= str.size(); j++)
         {
-            cout << str.substr(i, j) << endl;
+            if (isPalindrome(str.substr(i, j)))
+            {
+                cout << str.substr(i, j) << endl;
+            }
         }
     }
 
     return 0;
+}
+
+bool isPalindrome(string str)
+{
+    int low = 0;
+    int high = str.size() - 1;
+    while (low < high)
+    {
+        if (str[low] != str[high])
+        {
+            return false;
+        }
+        low++;
+        high--;
+    }
+    return true;
 }
