77d690f3ef67dd4d64c608d281d19b60e5a2b5b9
Vishal Tyagi <visualtaggy@gmail.com>
2022-07-03 21:46:52 +0530

    [Soln] First Function for compression

--- Basics Of Programming/4 String StringBuilder ArrayList/String Compression.cpp
+++ Basics Of Programming/4 String StringBuilder ArrayList/String Compression.cpp
@@ -4,3 +4,36 @@
 //    For "aaabbccdee", the compressed string will be "abcde".
 //    Second compression -> The string should be compressed such that consecutive duplicates of characters are replaced with the character and followed by the number of consecutive duplicates.
 //    For "aaabbccdee", the compressed string will be "a3b2c2de2".
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void compression1(string str);
+
+int main()
+{
+    string str;
+    getline(cin, str);
+
+    // Code
+    compression1(str);
+    cout << endl;
+
+    return 0;
+}
+
+void compression1(string str)
+{
+    int index = 0;
+    while (index < str.size())
+    {
+        while (str[index] == str[index + 1] && index < str.size() - 1)
+        {
+            ++index;
+        }
+        cout << str[index];
+        ++index;
+    }
+}
