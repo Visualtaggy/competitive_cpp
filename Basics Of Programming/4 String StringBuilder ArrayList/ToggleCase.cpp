7b0c4fcc4f459da8866ffd512c23a8bac53c057d
Vishal Tyagi <visualtaggy@gmail.com>
2022-07-04 22:13:26 +0530

    [Soln] Toggle Case in CPP

--- Basics Of Programming/4 String StringBuilder ArrayList/ToggleCase.cpp
+++ Basics Of Programming/4 String StringBuilder ArrayList/ToggleCase.cpp
@@ -2,11 +2,30 @@
 // 2. You have to toggle the case of every character of the given string.
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-   
+    // input
+    string str;
+    getline(cin, str);
+
+    // code
+    for (int i = 0; i < str.size(); i++)
+    {
+        char ch = str[i];
+
+        if (ch >= 'a' & ch <= 'z')
+        {
+            str[i] = 'A' + ch - 'a';
+        }
+        else
+        {
+            str[i] = 'a' + ch - 'A';
+        }
+    }
+    cout << str;
     return 0;
 }
