900d3d4da0d67e3286747d39995b4bf690b5df3f
Vishal Tyagi <visualtaggy@gmail.com>
2022-07-08 21:49:21 +0530

    [Soln] Print all permutation

--- Basics Of Programming/4 String StringBuilder ArrayList/Print All Permutations Of A String Iteratively.cpp
+++ Basics Of Programming/4 String StringBuilder ArrayList/Print All Permutations Of A String Iteratively.cpp
@@ -6,8 +6,43 @@
 
 using namespace std;
 
+int factorial(int number);
+
 int main()
 {
+    string str;
+    getline(cin, str);
 
+    int n = str.size();
+    int f = factorial(n);
+
+    for (int i = 0; i < f; i++)
+    {
+        string sb = str;
+        int temp = i;
+        for (int div = n; div >= 1; div--)
+        {
+            int q = temp / div;
+            int r = temp % div;
+
+            cout << sb[r];
+            sb.erase(r, 1);
+
+            temp = q;
+        }
+        cout << endl;
+    }
     return 0;
+}
+
+int factorial(int number)
+{
+    int answer = 1;
+
+    for (int i = 2; i <= number; i++)
+    {
+        answer *= i;
+    }
+
+    return answer;
 }
