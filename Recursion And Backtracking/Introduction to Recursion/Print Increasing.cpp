0e2a724bbb79d5d01ac77373ddd903cea97b6a2b
Vishal Tyagi <visualtaggy@gmail.com>
2022-07-09 21:11:01 +0530

    [Soln] : Print increasing

--- Recursion And Backtracking/Introduction to Recursion/Print Increasing.cpp
+++ Recursion And Backtracking/Introduction to Recursion/Print Increasing.cpp
@@ -10,6 +10,12 @@ using namespace std;
 void printIncreasing(int n)
 {
     // write your code here
+    if (n == 0)
+    {
+        return;
+    }
+    printIncreasing(n - 1);
+    cout << n << endl;
 }
 
 int main()
