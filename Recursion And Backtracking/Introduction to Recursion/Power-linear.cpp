fa5de407d60df151e19fddfb2dddb22cf84c7d9e
Vishal Tyagi <visualtaggy@gmail.com>
2022-07-11 22:23:59 +0530

    [Soln]: Recursion Power Linear

--- Recursion And Backtracking/Introduction to Recursion/Power-linear.cpp
+++ Recursion And Backtracking/Introduction to Recursion/Power-linear.cpp
@@ -3,3 +3,23 @@
 // 3. You are required to calculate x raised to the power n. Don't change the signature of power function .
 
 // Note -> The online judge can't force you to write the function recursively but that is what the spirit of question is. Write recursive and not iterative logic. The purpose of the question is to aid learning recursion and not test you.
+
+#include <iostream>
+using namespace std;
+
+int power(int x, int n)
+{
+    // write your code here
+    if (n == 0)
+    {
+        return 1;
+    }
+    return x * power(x, n - 1);
+}
+
+int main()
+{
+    int n, x;
+    cin >> x >> n;
+    cout << power(x, n);
+}
