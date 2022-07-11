9fd9d05a3be6e4fb83369133acab3b45278f03da
Vishal Tyagi <visualtaggy@gmail.com>
2022-07-11 23:23:53 +0530

    [Soln]:Recursion power log

--- Recursion And Backtracking/Introduction to Recursion/Power-logarithmic.cpp
+++ Recursion And Backtracking/Introduction to Recursion/Power-logarithmic.cpp
@@ -5,3 +5,35 @@
 // Note1 -> The previous version expects the call stack to be of n height. This function expects call function to be only log(n) high.
 
 // Note2 -> The online judge can't force you to write the function recursively but that is what the spirit of question is. Write recursive and not iterative logic. The purpose of the question is to aid learning recursion and not test you.
+
+#include <iostream>
+using namespace std;
+
+int powerLogarithmic(int x, int n)
+{
+    // base case
+    if (n == 0)
+    {
+        return 1;
+    }
+
+    int val = powerLogarithmic(x, n / 2);
+
+    if (n % 2 == 0)
+    {
+        //if power is even
+        return val * val;
+    }
+    else
+    {
+        //if power is odd
+        return val * val * x;
+    }
+}
+
+int main()
+{
+    int x, n;
+    cin >> x >> n;
+    cout << powerLogarithmic(x, n);
+}
