588f9a0b7c6329b11416eb9a90121393773c69f3
Vishal Tyagi <visualtaggy@gmail.com>
2022-07-12 23:26:40 +0530

    [Soln]:Recursion Tower of Hanoi

--- Recursion And Backtracking/Introduction to Recursion/Tower Of Hanoi.cpp
+++ Recursion And Backtracking/Introduction to Recursion/Tower Of Hanoi.cpp
@@ -7,6 +7,18 @@ using namespace std;
 
 void toh(int n, int t1id, int t2id, int t3id)
 {
+
+    if (n == 0)
+    {
+        return;
+    }
+
+    toh(n - 1, t1id, t3id, t2id);
+
+    // 1[10 -> 11]
+    cout << n << "[" << t1id << " -> " << t2id << "]" << endl;
+
+    toh(n - 1, t3id, t2id, t1id);
 }
 
 int main()
