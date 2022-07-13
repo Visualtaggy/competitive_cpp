e961934306307a5d887ab6f9d3d5b84052ec62ad
Vishal Tyagi <visualtaggy@gmail.com>
2022-07-13 19:26:22 +0530

    [Quest]:Recursive way to find max of an array

---
+++ Recursion And Backtracking/Recursion In Arrays/Max Of An Array.cpp
@@ -0,0 +1,25 @@
+// 1. You are given a number n, representing the count of elements.
+// 2. You are given n numbers.
+// 3. You are required to find the maximum of input.
+// 4. For the purpose complete the body of maxOfArray function. Don't change the signature.
+
+// Note -> The online judge can't force you to write the function recursively but that is what the spirit of question is. Write recursive and not iterative logic. The purpose of the question is to aid learning recursion and not test you.
+
+#include <iostream>
+using namespace std;
+
+int max(int arr[], int idx, int n)
+{
+    // write your code here
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    int p = max(arr, 0, n);
+    cout << p << endl;
+}
