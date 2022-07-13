d4b6e67deb4c19bb27248f3e3c903fd72791a7b1
Vishal Tyagi <visualtaggy@gmail.com>
2022-07-13 23:05:49 +0530

    adding boiler plat code

--- Recursion And Backtracking/Recursion In Arrays/Last Index.cpp
+++ Recursion And Backtracking/Recursion In Arrays/Last Index.cpp
@@ -1,7 +1,27 @@
 // 1. You are given a number n, representing the count of elements.
 // 2. You are given n numbers.
-// 3. You are given a number x. 
+// 3. You are given a number x.
 // 4. You are required to find the last index at which x occurs in array a.
 // 5. If x exists in array, print the last index where it is found otherwise print -1.
 
 // Note -> The online judge can't force you to write the function recursively but that is what the spirit of question is. Write recursive and not iterative logic. The purpose of the question is to aid learning recursion and not test you.
+#include <iostream>
+using namespace std;
+
+int lastIndex(int arr[], int idx, int x, int n)
+{
+    // write your code here
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int d;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    cin >> d;
+    int p = lastIndex(arr, 0, d, n);
+    cout << p << endl;
+}
