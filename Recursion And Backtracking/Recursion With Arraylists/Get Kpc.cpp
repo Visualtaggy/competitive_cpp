d2328fde6804259d78d368dce5b6743d01e5875f
Vishal Tyagi <visualtaggy@gmail.com>
2022-07-17 01:00:38 +0530

    [Quest]:Recursion list all words possible by keys

---
+++ Recursion And Backtracking/Recursion With Arraylists/Get Kpc.cpp
@@ -0,0 +1,44 @@
+// 1. You are given a string str. The string str will contains numbers only, where each number stands for a key pressed on a mobile phone.
+// 2. The following list is the key to characters map :
+//     0 -> .;
+//     1 -> abc
+//     2 -> def
+//     3 -> ghi
+//     4 -> jkl
+//     5 -> mno
+//     6 -> pqrs
+//     7 -> tu
+//     8 -> vwx
+//     9 -> yz
+// 3. Complete the body of getKPC function - without changing signature - to get the list of all words that could be produced by the keys in str.
+// Use sample input and output to take idea about output.
+
+// Note -> The online judge can't force you to write the function recursively but that is what the spirit of question is. Write recursive and not iterative logic. The purpose of the question is to aid learning recursion and not test you.
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+vector<string> getKPC(string s)
+{
+    // Write your code here
+}
+
+int main()
+{
+    string s;
+    cin >> s;
+    vector<string> ans = getKPC(s);
+    int cnt = 0;
+
+    cout << '[';
+    for (string str : ans)
+    {
+        if (cnt != ans.size() - 1)
+            cout << str << ", ";
+        else
+            cout << str;
+        cnt++;
+    }
+    cout << ']';
+}
