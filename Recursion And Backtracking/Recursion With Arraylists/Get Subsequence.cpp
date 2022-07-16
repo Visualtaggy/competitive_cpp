96bb293cd01b0b0b53c9c3c0b0ca66d0fde7a52c
Vishal Tyagi <visualtaggy@gmail.com>
2022-07-16 15:52:12 +0530

    [Quest] Recursive get string subsequence

---
+++ Recursion And Backtracking/Recursion With Arraylists/Get Subsequence.cpp
@@ -0,0 +1,34 @@
+// 1. You are given a string str.
+// 2. Complete the body of getSS function - without changing signature - to calculate all subsequences of str.
+// Use sample input and output to take idea about subsequences.
+
+// Note -> The online judge can't force you to write the function recursively but that is what the spirit of question is.
+// Write recursive and not iterative logic. The purpose of the question is to aid learning recursion and not test you.
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<string> gss(string s)
+{
+    // write your code here
+}
+
+int main()
+{
+    string s;
+    cin >> s;
+    vector<string> ans = gss(s);
+    int cnt = 0;
+
+    cout << '[';
+    for (string str : ans)
+    {
+        if (cnt != ans.size() - 1)
+            cout << str << ", ";
+        else
+            cout << str;
+        cnt++;
+    }
+    cout << ']';
+}
