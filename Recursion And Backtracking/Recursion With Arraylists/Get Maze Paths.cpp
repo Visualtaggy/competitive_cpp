ce94da510bc232ca35cd828cd90461a1855a3f34
Vishal Tyagi <visualtaggy@gmail.com>
2022-07-17 21:40:18 +0530

    [Quest]: Maze path reach destination recursively

---
+++ Recursion And Backtracking/Recursion With Arraylists/Get Maze Paths.cpp
@@ -0,0 +1,41 @@
+// 1. You are given a number n and a number m representing number of rows and columns in a maze.
+// 2. You are standing in the top-left corner and have to reach the bottom-right corner. Only two moves are allowed 'h' (1-step horizontal) and 'v' (1-step vertical).
+// 3. Complete the body of getMazePath function - without changing signature - to get the list of all paths that can be used to move from top-left to bottom-right.
+// Use sample input and output to take idea about output.
+
+// Note -> The online judge can't force you to write the function recursively but that is what the spirit of question is. Write recursive and not iterative logic. The purpose of the question is to aid learning recursion and not test you.
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// sr - source row
+// sc - source column
+// dr - destination row
+// dc - destination column
+vector<string> getMazePaths(int sr, int sc, int dr, int dc)
+{
+}
+
+void display(vector<string> &arr)
+{
+    cout << "[";
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cout << arr[i];
+        if (i < arr.size() - 1)
+            cout << ", ";
+    }
+
+    cout << "]" << endl;
+}
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    vector<string> ans = getMazePaths(0, 0, n - 1, m - 1);
+    display(ans);
+
+    return 0;
+}
