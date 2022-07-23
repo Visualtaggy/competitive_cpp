0a69e01ff80475902c752f5b098be5170b193e90
Vishal Tyagi <visualtaggy@gmail.com>
2022-07-23 16:24:28 +0530

    [Soln]: Recursion on the way up: maze paths

--- Recursion And Backtracking/Recursion On The Way Up/Print Maze Paths.cpp
+++ Recursion And Backtracking/Recursion On The Way Up/Print Maze Paths.cpp
@@ -10,7 +10,21 @@ using namespace std;
 
 void printMazePaths(int sr, int sc, int dr, int dc, string psf)
 {
+    if (sr == dr & sc == dc)
+    {
+        cout << psf << endl;
+        return;
+    }
+
     // write your code here
+    if (sc < dc)
+    {
+        printMazePaths(sr, sc + 1, dr, dc, psf + "h");
+    }
+    if (sr < dr)
+    {
+        printMazePaths(sr + 1, sc, dr, dc, psf + "v");
+    }
 }
 
 int main()
