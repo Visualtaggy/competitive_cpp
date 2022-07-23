19a85ef9ca065e48d88e18c0237152ddd0ebdb4d
Vishal Tyagi <visualtaggy@gmail.com>
2022-07-23 15:34:07 +0530

    [Soln]:Recursion on the way up: print stair paths

--- Recursion And Backtracking/Recursion On The Way Up/Print Stair Paths.cpp
+++ Recursion And Backtracking/Recursion On The Way Up/Print Stair Paths.cpp
@@ -10,6 +10,22 @@ using namespace std;
 void printStairPaths(int n, string psf)
 {
     // write your code here
+    // Reached destination
+    if (n == 0)
+    {
+        cout << psf << endl;
+        return;
+    }
+    // Crossed destination
+    if (n < 0)
+    {
+        return;
+    }
+
+    // recursive calls
+    printStairPaths(n - 1, psf + "1");
+    printStairPaths(n - 2, psf + "2");
+    printStairPaths(n - 3, psf + "3");
 }
 
 int main()
