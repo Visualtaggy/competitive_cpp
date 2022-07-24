129420e4b27a000a49c53b736f8d4cb470d8e79d
Vishal Tyagi <visualtaggy@gmail.com>
2022-07-24 15:21:19 +0530

    [Quest] Recursion on the way up - cout permutatins

---
+++ Recursion And Backtracking/Recursion On The Way Up/Print Permutations.cpp
@@ -0,0 +1,20 @@
+// 1. You are given a string str.
+// 2. Complete the body of printPermutations function - without changing signature - to calculate and print all permutations of str.
+// Use sample input and output to take idea about permutations.
+
+// Note -> The online judge can't force you to write the function recursively but that is what the spirit of question is. Write recursive and not iterative logic. The purpose of the question is to aid learning recursion and not test you.
+
+#include <iostream>
+using namespace std;
+
+void printPermutations(string str, string asf)
+{
+    // write your code here
+}
+
+int main()
+{
+    string str;
+    cin >> str;
+    printPermutations(str, "");
+}
