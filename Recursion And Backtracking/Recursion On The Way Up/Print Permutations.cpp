fcb499c93a8232121d1ff94039bacf446057f70a
Vishal Tyagi <visualtaggy@gmail.com>
2022-07-24 20:41:16 +0530

    [Soln] Recursion on the way up - print permutatins

--- Recursion And Backtracking/Recursion On The Way Up/Print Permutations.cpp
+++ Recursion And Backtracking/Recursion On The Way Up/Print Permutations.cpp
@@ -5,11 +5,35 @@
 // Note -> The online judge can't force you to write the function recursively but that is what the spirit of question is. Write recursive and not iterative logic. The purpose of the question is to aid learning recursion and not test you.
 
 #include <iostream>
+#include <string>
 using namespace std;
 
 void printPermutations(string str, string asf)
 {
     // write your code here
+    if (str.size() == 0)
+    {
+        cout << asf << endl;
+        return;
+    }
+
+    for (int i = 0; i < str.size(); i++)
+    {
+        // Removing 'a' from 'abc'
+        char ch = str[i];
+
+        // taking left side of the remaining string => 'b'
+        string left = str.substr(0, i);
+
+        // taking right side of the remaining string => 'c'
+        string right = str.substr(i + 1, str.size());
+
+        // Combining to form => 'ac'
+        string combined = left + right;
+
+        // recusive call
+        printPermutations(combined, asf + ch);
+    }
 }
 
 int main()
