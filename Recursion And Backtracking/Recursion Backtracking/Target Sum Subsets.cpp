9d9ff439b503f47a917d5bb5ff1507a4b7c9918f
Vishal Tyagi <visualtaggy@gmail.com>
2022-07-28 22:01:18 +0530

    [Quest]: Backtracking - target subsets

---
+++ Recursion And Backtracking/Recursion Backtracking/Target Sum Subsets.cpp
@@ -0,0 +1,22 @@
+// 1. You are given a number n, representing the count of elements.
+// 2. You are given n numbers.
+// 3. You are given a number "tar".
+// 4. Complete the body of printTargetSumSubsets function - without changing signature - to calculate and print all subsets of given elements, the contents of which sum to "tar". Use sample input and output to get more idea.
+
+// Note -> The online judge can't force you to write the function recursively but that is what the spirit of question is. Write recursive and not iterative logic. The purpose of the question is to aid learning recursion and not test you.
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// set is the subset
+// sos is sum of subset
+// tar is target
+void printTargetSumSubsets(vector<int> arr, int idx, string set, int sos, int tar)
+{
+    // write your code here
+}
+
+int main()
+{
+}
